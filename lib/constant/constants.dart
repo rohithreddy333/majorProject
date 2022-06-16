f5cb640c69ecb0e93b49960d308c3a0158bb5065
Rohith Reddy Dhoodhipala <50194466+rohithreddy333@users.noreply.github.com>
2022-06-16 21:39:41 +0530

    Add files via upload

---
+++ lib/constant/constants.dart
@@ -0,0 +1,13 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoicm9oaXRoY29kZXMiLCJhIjoiY2wzaGozMGt5MTloODNpb2ZkNG1sMzlmeCJ9.TG_m1M_3r2MmCSiS_fx0Ag';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGEwNjVGN0I2YjA2QjQ1MjVCODVGOGNBYzI0Mjk1ZTNGRjcwOTkwQTAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1MzIxNzY1NjM1MCwibmFtZSI6Im1ham9yIFByb2oifQ.VPS_Y1PpLgv1LoE3Fz6gJ45Kw5GqIs-JVVZC86aILnc';
+
+const String rpcUrl =
+    "http://127.0.0.1:7545";
+//"http://127.0.0.1:7545";
+
+const String contractAddress = "0x0b30eeD91CD9A095693C998B7ae1208568031ffD";
+
+const int chainId = 1337; //Ropsten-3, Polygon testnet-80001, local ganache-1337
